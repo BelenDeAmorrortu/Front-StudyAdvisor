9ad1f9c51e5896f26d932e0999de0ee4be40c16c
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-10-26 01:46:53 -0300

    (style) new Scss files + alerts styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Permanent/Nav';
 import Home from './components/Home/Home';
