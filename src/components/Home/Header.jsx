32c7635952612ff68bcf3d89432fcbd2eade4e6b
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-10-30 18:05:32 -0300

    (fix) log in button not visible when user has already logged in

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -4,8 +4,11 @@ import img from '../../images/undraw_add_tasks_re_s5yj.svg'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function Header(){
+
+    const {currentUser} = useAuth()
 
     useEffect(()=>{
         AOS.init({duration: 1000})
@@ -20,9 +23,14 @@ export default function Header(){
                 <div className={style.title_container}>
 
                     <h1>Study Advisor</h1>
+                    {!currentUser?
+
                     <NavLink to='/signIn'>
                         <button>Sign In</button>
                     </NavLink>
+
+                    :null
+                    }
                 </div>
 
                 <div className={style.image_container} data-aos='fade-left'>
@@ -31,7 +39,7 @@ export default function Header(){
 
             </div>
 
-            <div className={style.wave} style={{height: '150px', overflow: 'hidden'}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width: '100%'}}><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{stroke: 'none', fill: '#ffff'}}></path></svg></div>
+            <div className={style.wave} style={{height: '150px', overflow: 'hidden'}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width: '100%'}}><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{stroke: 'none'}}></path></svg></div>
 
         </div>
     )
