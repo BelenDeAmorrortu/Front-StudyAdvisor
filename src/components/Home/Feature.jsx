8adec9efa433bb41f1c19b198bb6183f78e1e335
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-11-09 19:34:16 -0300

    (style) new glass color variables

--- src/components/Home/Feature.jsx
+++ src/components/Home/Feature.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useTheme } from '../../contexts/ThemeContext'
 import style from '../../StyleSheets/Home/Feature.module.scss'
 
 export default function Feature({name, icon}) {
 
+    let {currentTheme} = useTheme()
+
     return (
 
-        <div className={style.feature_container}>
+        <div className={`${style.feature_container} ${style[currentTheme]}`}>
 
             <h4>{name}</h4>
             {icon}
--- src/components/Permanent/Card.jsx
+++ src/components/Permanent/Card.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import { useTheme } from '../../contexts/ThemeContext';
 import style from '../../StyleSheets/Permanent/Card.module.scss'
 
 export default function Card({name, NavLinkUrl, imgUrl, handleDelete, id}) {
 
+    let { currentTheme } = useTheme()
+    
     return (
 
         <div className={style.container}>
 
             <button onClick={()=> handleDelete(id)}>X</button>
 
-            <div className={style.card}>
+            <div className={`${style.card} ${style[currentTheme]}`}>
 
                 <NavLink to={NavLinkUrl} className={style.link}>
 
